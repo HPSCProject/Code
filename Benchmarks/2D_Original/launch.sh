5ecf6040d510b9e9f2dd98becefd7d875156c45d
jabr4442 <jasmine.brewer@colorado.edu>
2014-10-26 13:34:22 -0600

    Original 2D benchmark data, including all of the files you need to run the benchmarks

---
+++ Benchmarks/2D_Original/launch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=12
+#SBATCH --time=00:30:00
+#SBATCH --job-name=jasmine_project
+#SBATCH --output=project.out
+
+./8-16_breathing.exe 1001 1001 200 250 >> out.txt
