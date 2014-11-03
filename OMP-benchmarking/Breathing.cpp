9acf5e7618d00113566c2f45c1b17b72db56a2e3
sael9740 <samuel.elliott@colorado.edu>
2014-11-03 13:04:39 -0700

    benchmarking

--- OMP-benchmarking/Breathing.cpp
+++ OMP-benchmarking/Breathing.cpp
@@ -19,13 +19,9 @@ using namespace std;
 
 int main(int argc, const char* argv[])
 {
-    double write_start, write_time, eq_start, eq_time;
-	write_time = 0;
-	eq_time = 0;
-    double main_start = get_walltime();
-  int nx = 251;
-  int ny = 251;
-  int nsteps = 250;  // are these time steps??
+  int nx = 10;
+  int ny = 10;
+  int nsteps = 20;  // are these time steps??
   double sd = 50.0;   // standard deviation
   double omega = 1.0; // ??
 
@@ -46,9 +42,9 @@ int main(int argc, const char* argv[])
 
   double T0 = 1.0/3.0;
 
-  vector<vector<vector<double> > > fIn (nx, vector<vector<double> >(ny, vector<double>(Q, 0.0)));
+  vector<vector<vector <double> > > fIn (nx, vector<vector <double> >(ny, vector<double>(Q, 0.0)));
 
-  vector<vector<vector<double> > > fOut(nx, vector<vector<double> >(ny, vector<double>(Q, 0.0)));
+  vector<vector<vector<double> > > fOut(nx, vector<vector <double> >(ny, vector<double>(Q, 0.0)));
 
   vector<vector<double> > rho (nx, vector<double>(ny, 1.0));
   vector<vector<double> > ux (nx, vector<double>(ny, 0.0));
@@ -62,36 +58,26 @@ int main(int argc, const char* argv[])
   double w2 = 1.0 / 9.0;
   double w3 = 1.0 / 36.0;
   double wi[] = {w1, w2, w2, w2, w2, w3, w3, w3, w3};
-    double gaussian_start = get_walltime();
+
   init_gaussian(&fIn, &fOut, &rho, &ux, &uy, c, wi, lambda, nx, ny, sd, T0, omega);
-    double gaussian_time = get_walltime() - gaussian_start;
+ 
   for (int ts = 0; ts < nsteps; ++ts)
+  {
+    if (ts == t_on)
     {
-      if (ts == t_on)
-	{
-	  ftrue = true;
-	}
-      else if (ts == t_off)
-	{
-	  ftrue = false;
-	}
-        eq_start = get_walltime();
-      eq_and_stream(&fIn, &fOut, &rho, &ux, &uy, c, wi, nop, lambda, nx, ny, T0, omega, sd, ftrue);
-        eq_time += get_walltime() - eq_start;
-      fIn = fOut;
-      if(ts%10==0)
-          write_start = get_walltime();
-      write_gaussian(&rho, &ux, &uy, nx, ny, sd, ts);
-        write_time += get_walltime() - write_start;
+      ftrue = true;
+    }
+    else if (ts == t_off)
+    {
+      ftrue = false;
     }
 
-    double main_time = get_walltime() - main_start;
-    
-    cout << "For size " << nx << "x" << ny << " over " << nsteps << " Timesteps" << endl;
-    cout << "\tTotal Breathing Time: \t\t" << main_time << endl;
-    cout << "\tTotal Time For init_gaussian(): \t" << gaussian_time << endl;
-    cout << "\tTotal Time For eq_and_stream(): \t" << eq_time << endl;
-    cout << "\tTotal Time For write_gaussian(): \t" << write_time << endl;
-    
+    eq_and_stream(&fIn, &fOut, &rho, &ux, &uy, c, wi, nop, lambda, nx, ny, T0, omega, sd, ftrue);
+
+    fIn = fOut;
+
+    write_gaussian(&rho, &ux, &uy, nx, ny, sd, ts);
+  }
+
   return 0;
 }
