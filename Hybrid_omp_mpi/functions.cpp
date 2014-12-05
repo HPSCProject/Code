0c6669b9d67c2b79176fdf5d167bd4ba8c976f80
sael9740 <sael9740@colorado.edu>
2014-12-04 20:37:21 -0700

    transfer

--- Hybrid_omp_mpi/functions.cpp
+++ Hybrid_omp_mpi/functions.cpp
@@ -51,8 +51,7 @@ void eq_and_stream(qr_type ***fIn, qr_type ***fOut, qr_type **rho, qr_type **ux,
     for (int j = 0; j <= l_sz_J+1; ++j)
     {
         if(introduced) {
-            void a;
-            cout << "Hello I am process #" << my_rank << " my j is " << j << " thread #" << omp_get_thread_num(a) << endl;
+            cout << "Hello I am process #" << my_rank << " my j is " << j << " thread #" << omp_get_thread_num() << endl;
             introduced = false;
         }
       gl_y=j+bl_y-1;
